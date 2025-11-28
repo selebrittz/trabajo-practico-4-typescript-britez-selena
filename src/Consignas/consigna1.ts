789f411a1b349f659766a7d209038ae1ccb7d9fa
Selena Britez <selenabritez934@gmail.com>
2025-11-28 11:04:40 -0300

    Tipos Básicos, Unión e Interfaces realizados

---
+++ src/Consignas/consigna1.ts
@@ -0,0 +1,17 @@
+let productName: string = "Labial Maybelline";
+let productPrice: number = 9000;
+
+let productId: string | number;
+
+productId = 1;
+productId = "LM";
+
+interface Product {
+  name: string;
+  price: number;
+}
+
+let myproduct: Product = {
+  name: "Rimel Avon",
+  price: 20000,
+};
